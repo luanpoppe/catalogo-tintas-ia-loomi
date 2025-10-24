4ccead15662c703e2b2f9c61b7da0dc82ba3e413
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-24 18:51:40 -0300

    refactor: adicionar lógica da configuração da memória de curto prazo em um arquivo separado

--- src/lib/langchain/langchain.ts
+++ src/lib/langchain/langchain.ts
@@ -1,3 +1,4 @@
+import { ShortTermMemory } from "./short-term-memory";
 import { createAgent } from "langchain";
 import { LangchainModels } from "./models";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
@@ -10,13 +11,16 @@ type CreateAgentProps = {
 export class Langchain {
   static models = LangchainModels;
 
-  static createAgent({
+  static async createAgent({
     model,
     systemPrompt,
-  }: CreateAgentProps): ReturnType<typeof createAgent> {
+  }: CreateAgentProps): Promise<ReturnType<typeof createAgent>> {
+    const checkpointer = await ShortTermMemory.checkpointer();
+
     const agent = createAgent({
       model,
       systemPrompt: systemPrompt ?? "",
+      checkpointer,
     });
 
     return agent;
