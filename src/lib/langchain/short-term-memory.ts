bf5761d0094076ac81192226dd1d74f1bcd9afc9
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-24 22:38:17 -0300

    fix: alterar schema utilizado pela biblioteca de gerenciamento da memória de curto prazo dos agentes

--- src/lib/langchain/short-term-memory.ts
+++ src/lib/langchain/short-term-memory.ts
@@ -3,7 +3,9 @@ import { env } from "../../env";
 
 export class ShortTermMemory {
   static async checkpointer() {
-    const checkpointer = PostgresSaver.fromConnString(env.DATABASE_URL);
+    const checkpointer = PostgresSaver.fromConnString(env.DATABASE_URL, {
+      schema: "langgrapgh",
+    });
     await checkpointer.setup();
 
     return checkpointer;
