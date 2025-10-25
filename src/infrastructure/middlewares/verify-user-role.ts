540060fb1d1f6af7b831cb1fa16dee679d4457d3
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-25 18:05:55 -0300

    feat: adicionar middlewares que irão avaliar se usuário está autenticado e autorizado

---
+++ src/infrastructure/middlewares/verify-user-role.ts
@@ -0,0 +1,13 @@
+import { PERMISSOES } from "@/generated/prisma/enums";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export class VerifyUserRole {
+  async middleware(permissaoNecessaria: PERMISSOES) {
+    return (request: FastifyRequest, reply: FastifyReply) => {
+      const { tipoDeUsuario } = request.user;
+
+      if (tipoDeUsuario !== permissaoNecessaria)
+        return reply.status(403).send({ message: "Unauthorized." });
+    };
+  }
+}
