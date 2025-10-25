64d23b7a28df61f136cfa2107363dd027188de0c
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-25 18:17:40 -0300

    feat: adicionar middlewares de autenticação e autorização às rotas conforme a funcionalidade de cada rota

--- src/domains/tintas/infrastructure/http/controllers/tintas.router.ts
+++ src/domains/tintas/infrastructure/http/controllers/tintas.router.ts
@@ -6,18 +6,48 @@ import { GetTintaByIdDocs } from "../docs/get-tinta-by-id.docs";
 import { UpdateTintaDocs } from "../docs/update-tinta.docs";
 import { DeleteTintaDocs } from "../docs/delete-tinta.docs";
 import { GetTintasByQueryDocs } from "../docs/get-tintas-by-query.docs";
+import { VerifyJwtMiddleware } from "@/infrastructure/middlewares/verify-jwt.middleware";
+import { VerifyUserRole } from "@/infrastructure/middlewares/verify-user-role";
 
 export class TintasRouter {
   static async route(app: FastifyInstance) {
-    app.post("/", CreateTintaDocs as any, TintasController.create);
+    app.addHook("preHandler", VerifyJwtMiddleware.middleware);
+
     app.get("/", GetAllTintasDocs as any, TintasController.getAll);
+
     app.get(
       "/search",
       GetTintasByQueryDocs as any,
       TintasController.getByQuery
     );
+
     app.get("/:id", GetTintaByIdDocs as any, TintasController.getById);
-    app.patch("/:id", UpdateTintaDocs as any, TintasController.update);
-    app.delete("/:id", DeleteTintaDocs as any, TintasController.delete);
+
+    app.post(
+      "/",
+      {
+        ...(CreateTintaDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      TintasController.create
+    );
+
+    app.patch(
+      "/:id",
+      {
+        ...(UpdateTintaDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      TintasController.update
+    );
+
+    app.delete(
+      "/:id",
+      {
+        ...(DeleteTintaDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      TintasController.delete
+    );
   }
 }
--- src/domains/usuarios/infrastructure/http/controllers/usuarios.router.ts
+++ src/domains/usuarios/infrastructure/http/controllers/usuarios.router.ts
@@ -5,21 +5,51 @@ import { DeleteUsuarioDocs } from "../docs/delete-usuario.docs";
 import { GetUsuarioByIdDocs } from "../docs/get-usuario-by-id.docs";
 import { GetUsuarioByEmailDocs } from "../docs/get-usuario-by-email.docs";
 import { UpdateUsuarioDocs } from "../docs/update-usuario.docs";
+import { VerifyUserRole } from "@/infrastructure/middlewares/verify-user-role";
+import { VerifyJwtMiddleware } from "@/infrastructure/middlewares/verify-jwt.middleware";
 
 export class UsuariosRouter {
   static async route(app: FastifyInstance) {
-    app.post("/", CreateUsuarioDocs as any, UsuariosController.create);
-
-    app.delete("/:id", DeleteUsuarioDocs as any, UsuariosController.delete);
-
-    app.get("/:id", GetUsuarioByIdDocs, UsuariosController.getById as any);
+    app.get(
+      "/:id",
+      { ...GetUsuarioByIdDocs, preHandler: VerifyJwtMiddleware.middleware },
+      UsuariosController.getById as any
+    );
 
     app.get(
       "/email/:email",
-      GetUsuarioByEmailDocs as any,
+      {
+        ...(GetUsuarioByEmailDocs as any),
+        preHandler: VerifyJwtMiddleware.middleware,
+      },
       UsuariosController.getByEmail as any
     );
 
-    app.put("/:id", UpdateUsuarioDocs as any, UsuariosController.update);
+    app.post(
+      "/",
+      {
+        ...(CreateUsuarioDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      UsuariosController.create
+    );
+
+    app.delete(
+      "/:id",
+      {
+        ...(DeleteUsuarioDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      UsuariosController.delete
+    );
+
+    app.put(
+      "/:id",
+      {
+        ...(UpdateUsuarioDocs as any),
+        preHandler: VerifyUserRole.middleware("ADMIN"),
+      },
+      UsuariosController.update
+    );
   }
 }
