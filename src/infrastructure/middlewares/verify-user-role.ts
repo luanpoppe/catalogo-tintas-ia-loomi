f4376d8e1b4baa6f07e3fdc5bd531e3caaa9996b
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-25 18:10:52 -0300

    fix: tornar método do middleware de verificação de permissão em estático

--- src/infrastructure/middlewares/verify-user-role.ts
+++ src/infrastructure/middlewares/verify-user-role.ts
@@ -2,7 +2,7 @@ import { PERMISSOES } from "@/generated/prisma/enums";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export class VerifyUserRole {
-  async middleware(permissaoNecessaria: PERMISSOES) {
+  static async middleware(permissaoNecessaria: PERMISSOES) {
     return (request: FastifyRequest, reply: FastifyReply) => {
       const { tipoDeUsuario } = request.user;
 
