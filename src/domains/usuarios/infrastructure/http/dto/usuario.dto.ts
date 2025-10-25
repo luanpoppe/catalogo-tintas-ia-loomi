3467829e4bd1ace9c87a860cc1b212d1a35b8d67
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-25 15:06:40 -0300

    feat: melhorar a validação da senha no DTO de request de usuário

--- src/domains/usuarios/infrastructure/http/dto/usuario.dto.ts
+++ src/domains/usuarios/infrastructure/http/dto/usuario.dto.ts
@@ -1,8 +1,18 @@
 import z from "zod";
 import { UsuarioSchema } from "@/domains/usuarios/domain/entities/usuario.entity";
 
+const regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$/;
+
 export const RequestUsuarioDTOSchema = UsuarioSchema.extend({
-  password: z.string().min(8).max(25).nonempty(),
+  password: z
+    .string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres.")
+    .max(25, "A senha deve ter no máximo 25 caracteres.")
+    .nonempty("A senha não pode estar vazia")
+    .regex(
+      regexSenha,
+      "A senha deve conter pelo menos um número, uma letra maiúscula e uma minúscula."
+    ),
 }).omit({ id: true });
 
 export type RequestUsuarioDTO = z.infer<typeof RequestUsuarioDTOSchema>;
