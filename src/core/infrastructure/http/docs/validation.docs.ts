5010685a96b573e60eeadc906f2e2fccae42485d
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-26 17:50:06 -0300

    chore: adicionar uma documentação de erro padrão para erros de validação + Adicionar erro de validação à documentação da criação de tintas

---
+++ src/core/infrastructure/http/docs/validation.docs.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const ValidationDocs = z.object({
+  error: z.array(
+    z
+      .object({
+        keyword: z.string(),
+        instancePath: z.string(),
+        schemaPath: z.string(),
+        params: z.object(),
+        message: z.string().optional(),
+      })
+      .describe("Bad Request - Erro de validação")
+  ),
+});
--- src/domains/tintas/infrastructure/http/docs/create-tinta.docs.ts
+++ src/domains/tintas/infrastructure/http/docs/create-tinta.docs.ts
@@ -1,13 +1,15 @@
 import { RouteShorthandOptions } from "fastify";
 import z from "zod";
 import { ResponseTintaDTOSchema } from "../dto/tinta.dto";
+import { ValidationDocs } from "@/core/infrastructure/http/docs/validation.docs";
 
 export const CreateTintaDocs: RouteShorthandOptions = {
   schema: {
     tags: ["tintas"],
     description: "Criar uma tinta.",
     response: {
-      200: ResponseTintaDTOSchema.describe("Success"),
+      200: ResponseTintaDTOSchema.describe("Successo"),
+      400: ValidationDocs,
       500: z
         .object({
           error: z.literal("Unkown Error"),
