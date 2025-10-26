2883fe2cf453eb9a616f52efdd3b0729f1d6588e
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-26 18:39:20 -0300

    feat: adicionar manejo de exceção em caso de usuário não encontrado pelo id

--- src/domains/usuarios/application/use-cases/get-usuario-by-id.use-case.ts
+++ src/domains/usuarios/application/use-cases/get-usuario-by-id.use-case.ts
@@ -1,3 +1,4 @@
+import { RecursoNaoEncontradoException } from "@/core/exceptions/recurso-nao-encontrado.exception";
 import { IUsuarioRepository } from "../../domain/repositories/usuario.repository";
 
 export class GetUsuarioByIdUseCase {
@@ -5,6 +6,8 @@ export class GetUsuarioByIdUseCase {
 
   async execute(id: number) {
     const usuario = await this.usuarioRepository.findById(id);
+
+    if (!usuario) throw new RecursoNaoEncontradoException();
 
     return { usuario };
   }
