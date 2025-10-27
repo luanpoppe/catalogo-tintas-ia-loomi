1e10b6d738f71c83ea687841c698d7cb875b060b
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-27 00:45:34 -0300

    test: corrigir o builder de criação de usuário administrador para testes e2e

--- test/e2e/@e2e-builders/usuarios.builder.ts
+++ test/e2e/@e2e-builders/usuarios.builder.ts
@@ -7,6 +7,9 @@ import {
   RequestUsuarioDTO,
   ResponseUsuarioDTO,
 } from "@/domains/usuarios/infrastructure/http/dto/usuario.dto";
+import { BCryptJS } from "@/lib/encrypt/bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import request from "supertest";
 
 export class UsuariosBuilder {
@@ -32,24 +35,20 @@ export class UsuariosBuilder {
   }
 
   static async criarUsuarioAdmin() {
-    const email = "abcAdmin@gmail.com";
     const senha = "Senha123abc";
+    const encryptService = new BCryptJS();
+    const senhaHashed = await encryptService.hash(senha);
 
-    const requestBody: RequestUsuarioDTO = {
-      nome: "abc",
-      email: email,
-      senha,
-      tipoUsuario: "ADMIN",
-    };
+    const admin = await prisma.usuarios.create({
+      data: {
+        email: "abcAdmin@gmail.com",
+        nome: "abc",
+        tipoUsuario: "ADMIN",
+        passwordHash: senhaHashed,
+      },
+    });
 
-    const { body } = await request(app.server)
-      .post("/usuario")
-      .send(requestBody);
-
-    return {
-      usuario: body as ResponseUsuarioDTO,
-      senha,
-    };
+    return { admin, senha };
   }
 
   static async criarELogarUsuarioComum() {
@@ -76,11 +75,11 @@ export class UsuariosBuilder {
   }
 
   static async criarELogarUsuarioAdmin() {
-    const usuarioCriado = await this.criarUsuarioAdmin();
+    const { admin, senha } = await this.criarUsuarioAdmin();
 
     const requestBody: RequestLoginDTO = {
-      email: usuarioCriado.usuario.email,
-      senha: usuarioCriado.senha,
+      email: admin.email,
+      senha: senha,
     };
 
     const resposta = await request(app.server)
@@ -92,7 +91,7 @@ export class UsuariosBuilder {
     const { accessToken }: ResponseLoginDTO = resposta.body;
 
     return {
-      ...usuarioCriado,
+      usuarioCriado: admin,
       accessToken,
       cookies,
     };
