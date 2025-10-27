63323c4b188dfeb912425e2ac1957132d688d8e4
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-27 16:41:26 -0300

    chore: mover lib do langchain do package de api para o package de agente de tintas

--- packages/agente-tintas-ia/src/main.ts
+++ packages/agente-tintas-ia/src/main.ts
@@ -1,5 +1,24 @@
+import { Langchain } from "./lib/langchain/langchain";
+
 export class AgenteTintaIA {
-  log() {
+  async log() {
     console.log("Executou agenda de IA com sucesso");
+
+    // const model = Langchain.models.gemini();
+
+    // try {
+    //   const agent = await Langchain.createAgent({ model });
+    //   const res = await agent.invoke(
+    //     {
+    //       messages: [{ role: "user", content: (req.body as any).content }],
+    //     },
+    //     { configurable: { thread_id: "1" } }
+    //   );
+
+    // return reply.status(200).send({ msg: "Hello", res });
+    // } catch (error: any) {
+    //   console.error(error);
+    //   throw new Error();
+    // }
   }
 }
