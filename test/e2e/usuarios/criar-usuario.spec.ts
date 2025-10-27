a989c6b3af9b908c7ee5df3e6784bf78ddd9345f
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-27 00:52:22 -0300

    test: adicionar testes e2e de criação de usuário

--- test/e2e/usuarios/criar-usuario.spec.ts
+++ test/e2e/usuarios/criar-usuario.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "@/app";
 import { RequestUsuarioDTO } from "@/domains/usuarios/infrastructure/http/dto/usuario.dto";
+import { UsuariosBuilder } from "../@e2e-builders/usuarios.builder";
 
 describe("Criar usuário", () => {
   beforeAll(async () => {
@@ -11,7 +12,7 @@ describe("Criar usuário", () => {
     await app.close();
   });
 
-  it("deve ser possível criar um usuário", async () => {
+  it("deve ser possível criar um usuário comum", async () => {
     const email = "abc@gmail.com";
     const nome = "abc";
     const senha = "Senha123abc";
@@ -38,5 +39,50 @@ describe("Criar usuário", () => {
 
     expect(resposta.body.passwordHash).toBeUndefined();
     expect(resposta.body.senha).toBeUndefined();
+  });
+
+  it("não deve ser possível criar um usuário administrador sem ser um usuário administrador", async () => {
+    const email = "abcAdmin@gmail.com";
+    const nome = "abc";
+    const senha = "Senha123abc";
+    const tipoUsuario = "ADMIN";
+
+    const body: RequestUsuarioDTO = {
+      nome,
+      email,
+      senha,
+      tipoUsuario,
+    };
+
+    const resposta = await request(app.server).post("/usuario").send(body);
+
+    expect(resposta.statusCode).toEqual(403);
+    expect(resposta.body).toEqual({ error: expect.any(String) });
+  });
+
+  it("deve ser possível um usuário administrador criar outro usuário administrador", async () => {
+    const email = "abcAdmin123@gmail.com";
+    const nome = "abc";
+    const senha = "Senha123abc";
+    const tipoUsuario = "ADMIN";
+
+    const { accessToken, cookies } =
+      await UsuariosBuilder.criarELogarUsuarioAdmin();
+
+    const body: RequestUsuarioDTO = {
+      nome,
+      email,
+      senha,
+      tipoUsuario,
+    };
+
+    const resposta = await request(app.server)
+      .post("/usuario")
+      .set("Cookie", cookies)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send(body);
+
+    expect(resposta.statusCode).toEqual(403);
+    expect(resposta.body).toEqual({ error: expect.any(String) });
   });
 });
